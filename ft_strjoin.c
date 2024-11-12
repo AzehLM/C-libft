7d8a844b239efc5ce33a203e7deb4094d21eaa23
Guillaume Ebersohl <guigui1991@live.fr>
2024-11-12 09:04:55 +0100

    Changed size_t initialization value to 0 for strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: gueberso <gueberso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 21:03:36 by gueberso          #+#    #+#             */
-/*   Updated: 2024/11/05 21:13:34 by gueberso         ###   ########.fr       */
+/*   Updated: 2024/11/12 09:03:44 by gueberso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,13 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	res = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2)) + 1);
 	if (!res)
 		return (NULL);
-	i = -1;
+	i = 0;
 	j = 0;
-	while (s1 && s1[++i])
+	while (s1 && s1[i])
+	{
 		res[i] = s1[i];
+		i++;
+	}
 	while (s2 && s2[j])
 	{
 		res[i] = s2[j];
