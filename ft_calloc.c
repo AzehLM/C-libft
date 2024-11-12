19eb608ad2a9c8691ac3905de80f831eba2ecf3f
Guillaume Ebersohl <guigui1991@live.fr>
2024-11-12 09:13:50 +0100

    New variable in calloc for optimisation

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: gueberso <gueberso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 14:24:44 by gueberso          #+#    #+#             */
-/*   Updated: 2024/11/08 16:02:33 by gueberso         ###   ########.fr       */
+/*   Updated: 2024/11/12 09:12:10 by gueberso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,15 @@
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*array;
+	size_t	len_malloc;
 
 	if ((nmemb > SIZE_MAX_SQRT || size > SIZE_MAX_SQRT)
 		&& nmemb > 0 && (SIZE_MAX / nmemb) < size)
 		return (NULL);
-	array = malloc(nmemb * size);
+	len_malloc = nmemb * size;
+	array = malloc(len_malloc);
 	if (!array)
 		return (NULL);
-	ft_bzero(array, nmemb * size);
+	ft_bzero(array, len_malloc);
 	return (array);
 }
